23a2eaabb0a6ff665484593228a79d110ddf1657
Amr Eldib <amr@amreldib.com>
2016-11-27 10:32:36 -0800

    Updated how filter rules are added

--- powerline_prompt.lua
+++ powerline_prompt.lua
@@ -8,6 +8,94 @@ function lambda_prompt_filter()
     clink.prompt.value = string.gsub(new_value, "{lamb}", "λ")
 end
 
+--- copied from clink.lua
+ -- Resolves closest directory location for specified directory.
+ -- Navigates subsequently up one level and tries to find specified directory
+ -- @param  {string} path    Path to directory will be checked. If not provided
+ --                          current directory will be used
+ -- @param  {string} dirname Directory name to search for
+ -- @return {string} Path to specified directory or nil if such dir not found
+local function get_dir_contains(path, dirname)
+
+    -- return parent path for specified entry (either file or directory)
+    local function pathname(path)
+        local prefix = ""
+        local i = path:find("[\\/:][^\\/:]*$")
+        if i then
+            prefix = path:sub(1, i-1)
+        end
+        return prefix
+    end
+
+    -- Navigates up one level
+    local function up_one_level(path)
+        if path == nil then path = '.' end
+        if path == '.' then path = clink.get_cwd() end
+        return pathname(path)
+    end
+
+    -- Checks if provided directory contains git directory
+    local function has_specified_dir(path, specified_dir)
+        if path == nil then path = '.' end
+        local found_dirs = clink.find_dirs(path..'/'..specified_dir)
+        if #found_dirs > 0 then return true end
+        return false
+    end
+
+    -- Set default path to current directory
+    if path == nil then path = '.' end
+
+    -- If we're already have .git directory here, then return current path
+    if has_specified_dir(path, dirname) then
+        return path..'/'..dirname
+    else
+        -- Otherwise go up one level and make a recursive call
+        local parent_path = up_one_level(path)
+        if parent_path == path then
+            return nil
+        else
+            return get_dir_contains(parent_path, dirname)
+        end
+    end
+end
+
+-- copied from clink.lua
+-- clink.lua is saved under %CMDER_ROOT%\vendor
+local function get_hg_dir(path)
+    return get_dir_contains(path, '.hg')
+end
+
+-- adopted from clink.lua
+-- clink.lua is saved under %CMDER_ROOT%\vendor
+function colorful_hg_prompt_filter()
+
+    -- Colors for mercurial status
+    local colors = {
+        clean = "\x1b[1;37;40m",
+        dirty = "\x1b[31;1m",
+    }
+
+    if get_hg_dir() then
+        -- if we're inside of mercurial repo then try to detect current branch
+        local branch = get_hg_branch()
+        if branch then
+            -- Has branch => therefore it is a mercurial folder, now figure out status
+            if get_hg_status() then
+                color = colors.clean
+            else
+                color = colors.dirty
+            end
+
+            clink.prompt.value = string.gsub(clink.prompt.value, "{hg}", color.."("..branch..")")
+            return false
+        end
+    end
+
+    -- No mercurial present or not in mercurial file
+    clink.prompt.value = string.gsub(clink.prompt.value, "{hg}", "")
+    return false
+end
+
 -- copied from clink.lua
 -- clink.lua is saved under %CMDER_ROOT%\vendor
 local function get_git_dir(path)
@@ -90,5 +178,6 @@ function colorful_git_prompt_filter()
 end
 
 -- override the built-in filters
-clink.prompt.register_filter(lambda_prompt_filter, 1)
-clink.prompt.register_filter(colorful_git_prompt_filter, 50)
+clink.prompt.register_filter(lambda_prompt_filter, 55)
+clink.prompt.register_filter(colorful_hg_prompt_filter, 60)
+clink.prompt.register_filter(colorful_git_prompt_filter, 60)
